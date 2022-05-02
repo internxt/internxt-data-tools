816e6ce7d9f7faa7edaf8ec42faa4f5d43f39fe1
Joan Mora <elcorreudejoanmora@gmail.com>
2022-05-02 16:28:07 +0200

    Automatically store extraction metadata

--- internxt_data_tools/BigQuery.py
+++ internxt_data_tools/BigQuery.py
@@ -1,5 +1,6 @@
 from google.oauth2 import service_account
 from google.cloud import bigquery
+import pandas as pd
 
 
 class BigQuery:
@@ -27,7 +28,14 @@ class BigQuery:
     )
     return dataframe
 
-  def latest_metadata(self, metadata_table='inxt-analytics.metadata.load_history', table_name='stripe_extraction.charges'):
+  def latest_metadata(self, table_name, metadata_table='inxt-analytics.metadata.load_history'):
     query = f"""SELECT * FROM `{metadata_table}` WHERE table_name like '{table_name}' ORDER BY timestamp DESC LIMIT 1"""
     df = self.download_table(query)
     return df.iloc[0]
+
+  def store_extraction_metadata(self,table_name,  init_date, end_date, extraction_status, metadata_table='metadata.load_history'):
+    d = {'table_name': [table_name], 'timestamp': [end_date], 'timestamp_init': [init_date], 'status': [extraction_status]}
+    df = pd.DataFrame(data=d)
+    job = self.store_dataframe(df,metadata_table)
+    return job
+
