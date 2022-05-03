0aa8960a9eb36d65c85be15ea7884418b62d6744
Joan Mora <elcorreudejoanmora@gmail.com>
2022-05-03 20:51:12 +0200

    Invoices schema

--- internxt_data_tools/Schemas.py
+++ internxt_data_tools/Schemas.py
@@ -2,3 +2,4 @@ from .charges_schema import charges_schema
 from .customer_schema import customer_schema
 from .subscription_schema import subscription_schema
 from .prices_schema import price_schema
+from .invoices_schema import invoice_schema
