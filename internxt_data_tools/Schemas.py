e348bdb3ccc879ddafff0416feda3b51e773e7ff
Joan Mora <elcorreudejoanmora@gmail.com>
2022-05-03 19:52:28 +0200

    Define price schema

--- internxt_data_tools/Schemas.py
+++ internxt_data_tools/Schemas.py
@@ -1,3 +1,4 @@
 from .charges_schema import charges_schema
 from .customer_schema import customer_schema
 from .subscription_schema import subscription_schema
+from .prices_schema import price_schema
---
+++ internxt_data_tools/prices_schema.py
@@ -0,0 +1,9 @@
+price_schema = [
+  'id',
+  'nickname',
+  'product',
+  'unit_amount',
+  'metadata_name',
+  'metadata_planType',
+  'metadata_recurrence'
+]
